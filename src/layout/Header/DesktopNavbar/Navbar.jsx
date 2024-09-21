98998aa9575c3fcd7e015cf29e68799ba06f36f2
elmin007 <imanverdiyevemin414@gmail.com>
2024-09-21 23:58:32 +0400

    Added category

--- src/layout/Header/DesktopNavbar/Navbar.jsx
+++ src/layout/Header/DesktopNavbar/Navbar.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './../DesktopNavbar/style.css'
 import { BiSolidCategoryAlt } from "react-icons/bi";
-import { IoSearchSharp,IoAddSharp,IoFilter  } from "react-icons/io5";
-import HeaderFilterCard from '../headerFilterCard/HeaderFilterCard';
-import { useNavigate } from 'react-router-dom';
+import { IoSearchSharp, IoAddSharp, IoFilter } from "react-icons/io5";
+import { FaCar, FaHome, FaTshirt, FaCouch } from 'react-icons/fa';
 
 export default function Navbar() {
     const [selectedCity, setSelectedCity] = useState('');
-    const [showFilter, setShowFilter] = useState(false);
-    const navigate=useNavigate()
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState(null);
 
     const cities = ['Bakı', 'Gəncə', 'Sumqayıt', 'Şəki', 'Lənkəran'];
 
+
+    const categories = [
+        { name: 'Nəqliyyat', products: ['Skuter', 'Avtomobil', 'Motosiklet'], icon: <FaCar /> },
+        { name: 'Daşınmaz Əmlak', products: ['Mənzil', 'Ev', 'Ofis'], icon: <FaHome /> },
+        { name: 'Dekor', products: ['Güzgü', 'Lampa', 'Divar bəzəkləri'], icon: <FaCouch /> },
+        { name: 'Geyim', products: ['Köynək', 'Şalvar', 'Ayaqqabı'], icon: <FaTshirt /> }
+    ];
+
     const handleCityChange = (event) => {
-        setSelectedCity(event.target.value); 
+        setSelectedCity(event.target.value);
     };
 
-    const toggleFilterCard = () => {
-        setShowFilter(prev => !prev);
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
     };
 
     return (
         <>
-            <nav className="navbar navbar-expand-xl bg-light  align-items-center">
+            <nav className="navbar navbar-expand-xl bg-light align-items-center">
                 <div className="container">
-                    <a className="navbar-brand" onClick={()=>navigate('/')}>EasySaleApp</a>
+                    <a className="navbar-brand" href="#">EasySaleApp</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -35,7 +47,7 @@ export default function Navbar() {
                             <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
                         <div className="offcanvas-body">
-                            <div className="category-box">
+                            <div className="category-box" onClick={openModal}>
                                 <BiSolidCategoryAlt className='icon' /><span>Kategoriya</span>
                             </div>
                             <div className="input-group">
@@ -48,16 +60,50 @@ export default function Navbar() {
                                     ))}
                                 </select>
                                 <input placeholder="Əşya və ya xidmət axtarışı" type="text" className='search-input' />
-                                <button className='search-btn'><IoSearchSharp className='icon'/></button>
+                                <button className='search-btn'><IoSearchSharp className='icon' /></button>
                             </div>
                             <div className="adverts-box">
-                                <button href="#"><IoAddSharp className='icon'/>Yeni Elan</button>
-                                <button href="#" onClick={toggleFilterCard}><IoFilter className='icon'/> Filter</button>
+                                <a href="#"><span><IoAddSharp className='icon' />Yeni Elan</span></a>
+                                <a href="#"><span><IoFilter className='icon' /> Filter</span></a>
                             </div>
                         </div>
                     </div>
                 </div>
             </nav>
+            {isModalOpen && (
+                <div className="modal category-modal">
+                    <div className="modal-content">
+                        <button onClick={closeModal}>X</button>
+                        <div className="modal-body d-flex">
+                            <div className="categories">
+                                <div className="categories">
+                                    {categories.map((category, index) => (
+                                        <div
+                                            key={index}
+                                            onMouseEnter={() => setSelectedCategory(category)}
+                                            className="category-item"
+                                        >
+                                            <span className="category-icon">{category.icon}</span> {/* Icon */}
+                                            {category.name}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="products">
+                                {selectedCategory ? (
+                                    <ul>
+                                        {selectedCategory.products.map((product, index) => (
+                                            <li key={index}>{product}</li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p>Məhsulları görmək üçün kateqoriya seçin</p>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
-    )
+    );
 }
